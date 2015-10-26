d1eadc2078efbca3cfa8cfbba60ea15a10135c3b
landwulf <walkerluke8@gmail.com>
2015-10-26 13:26:57 +0000

    updated probability and cleaned some code
    still in need of a proper procedure for behavior, navigating the map, displaying the map, sending the map to pc, and calibration.

--- COMP329ASSIGNMENT1.java
+++ COMP329ASSIGNMENT1.java
@@ -7,80 +7,142 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class COMP329ASSIGNMENT1
 {
 	DifferentialPilot pilot;
-	OdometryPoseProvider opp = new OdometryPoseProvider(pilot); //pose provider now listens to pilot and updates accordingly
-			
-	TouchSensor leftBump = new TouchSensor(SensorPort.S2);
-	TouchSensor rightBump = new TouchSensor(SensorPort.S1);
-	UltrasonicSensor us = new UltrasonicSensor(SensorPort.S3);
-	
-	ourMap = new int[15][7];	
-
-	bool finished = false;
-	//create ultrasonic sensor here
-	  
-	public void senseArea()
-	{
-		//if we put probabilityUpdate on the pc could we have the pc work with and update the map in its own time while the robot moves around
-		//again? we could use threads, one to sends data to and recieves visual data from probabilityUpdate on the pc, while the robot
-		//deals with moving to the next location through another thread if so the routine would maybe be something like
-		
-		pilot.rotate(-90);
-		sensorResult = us.getDistance();
-		ourMap[x,y + 1] = probabilityUpdate(sensorResult);
-		
-		pilot.rotate(-90);
-		sensorResult = us.getDistance();
-		ourMap[x - 1,y] = probabilityUpdate(sensorResult);
-		
-		pilot.rotate(-90);
-		sensorResult = us.getDistance();
-		ourMap[x,y - 1] = probabilityUpdate(sensorResult);
-		
-		pilot.rotate(-90);
-		sensorResult = us.getDistance();
-		ourMap[x + 1,y] = probabilityUpdate(sensorResult);					
-	}
   
-	//could we move this class to be computed on the pc and not on the device?
-	public static void probabilityUpdate(int sensorResult)
+	COMP329ASSIGNMENT1()
 	{
-		//distance could be the difference in pose 
+		OdometryPoseProvider opp = new OdometryPoseProvider(pilot); //pose provider now listens to pilot and updates accordingly
+				
+		TouchSensor leftBump = new TouchSensor(SensorPort.S2);
+		TouchSensor rightBump = new TouchSensor(SensorPort.S1);
+		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S3);
 		
-		//update map visual here also?
-	  
-	}
+		//colour sensor, could we set an even to fire when it detects a certain colour?
+		
+		//check if we have a compass as well
+		
+		ourMap = new int[10][10]; //create our grid map, each grid spot is 25cm by 25cm
+		mapCount = new int[10][10]; //this array stores the number of times that grid location has been checked
+
+		for(int i = 0; i<10; i++)
+		{
+			for(int j = 0; j<10; j++)
+			{
+				ourMap[i][j] = 0;
+				mapCount[i][j] = 0;
+			}		
+		}
+		
+		bool finished = false;		
+	}	
+	    
+	public void senseArea()
+	{	
+		int gridDistance;
+		float sensorResult;
+		
+		//will probably need to add a try catch block incase we go out of range in this matrix's
+		
+		//check ahead of us
+		sensorResult = us.getDistance();
+		gridDistance = Math.round(sensorResult / 25);
+		mapCount[x,y + gridDistance]++;
+		ourMap[x,y + gridDistance] = ((++ourMap[x,y + gridDistance] + mapCount[x,y + gridDistance]) / 2 * mapCount[x,y + gridDistance]);
+		
+		gridDistance--; 
+		for(gridDistance >= 0; gridDistance--)
+		{
+			mapCount[x,y + gridDistance]++;
+			ourMap[x,y + gridDistance] = ((--ourMap[x,y + gridDistance] + mapCount[x,y + gridDistance]) / 2 * mapCount[x,y + gridDistance]);
+		}
 			
-	public static void main(String[] args)
-	{		    
-		//would be worth calibrating here to check the margin of error with expected and actual rotation
+		//check to our immediate right
+		pilot.rotate(-90);	
+		sensorResult = us.getDistance();
+		gridDistance = Math.round(sensorResult / 25);
+		mapCount[x + gridDistance,y]++;
+		ourMap[x + gridDistance,y] = ((++ourMap[x + gridDistance,y] + mapCount[x + gridDistance,y]) / 2 * mapCount[x + gridDistance,y]);
 		
+		gridDistance--; 
+		for(gridDistance >= 0; gridDistance--)
+		{
+			mapCount[x + gridDistance,y]++;
+			ourMap[x + gridDistance,y] = ((--ourMap[x + gridDistance,y] + mapCount[x + gridDistance,y]) / 2 * mapCount[x + gridDistance,y]);
+		}
+		
+		//check behind us
+		pilot.rotate(-90);	
+		sensorResult = us.getDistance();
+		gridDistance = Math.round(sensorResult / 25);
+		mapCount[x,y - gridDistance]++;
+		ourMap[x,y - gridDistance] = ((++ourMap[x,y - gridDistance] + mapCount[x,y - gridDistance]) / 2 * mapCount[x,y - gridDistance]);
+		
+		gridDistance--; 
+		for(gridDistance >= 0; gridDistance--)
+		{
+			mapCount[x,y - gridDistance]++;
+			ourMap[x,y - gridDistance] = ((--ourMap[x,y - gridDistance] + mapCount[x,y - gridDistance]) / 2 * mapCount[x,y - gridDistance]);
+		}
+		
+		//check to our immediate left
+		pilot.rotate(-90);	
+		sensorResult = us.getDistance();
+		gridDistance = Math.round(sensorResult / 25);
+		mapCount[x - gridDistance,y]++;
+		ourMap[x - gridDistance,y] = ((++ourMap[x - gridDistance,y] + mapCount[x - gridDistance,y]) / 2 * mapCount[x - gridDistance,y]);
+		
+		gridDistance--; 
+		for(gridDistance >= 0; gridDistance--)
+		{
+			mapCount[x - gridDistance,y]++;
+			ourMap[x - gridDistance,y] = ((--ourMap[x - gridDistance,y] + mapCount[x - gridDistance,y]) / 2 * mapCount[x - gridDistance,y]);
+		}	
+		
+		pilot.rotate(-90);
+	}
+  	
+	//send an x, y location here and we can try to navigate the grid to take the robot to it.
+	public static void navigateTO()
+	{
+		//look up grid based navigation
+		//http://cgi.csc.liv.ac.uk/~rmw/329/notes/lect12.pdf
+		
+		//might be worth always turning to the right first, then to the left as it finishes, try to cancel out the offset errors
+		//move in our desired x location first, then y.
+	}
+	
+	public static void calibration()
+	{
+		//do some rotations to improve reliability
+	}
+	
+	public static void main(String[] args)
+	{		    		
 		COMP329ASSIGNMENT1 robot = new COMP329ASSIGNMENT1(new DifferentialPilot(2.25f, 5.5f, Motor.B, Motor.C));
 	  
 		Button.ENTER.waitForPress();
 		   
+		robot.calibration();
+
+		Button.ENTER.waitForPress();
+		   
+		robot.senseArea();
+   	   
 		while(finished != true)
 		{
-			//spin around and test in the 4 nearby grids with the sensor
+			robot.navigateTO();
+			
 			robot.senseArea();
 			
+			//update map on device, send map to pc for display
 			
-			//move forward, we need to decide how to move, should we just move in a random direction avoiding definitely blocked locations?
-			//if we hit something with the bumpers, an object is definitely there, 
-			// back up a bit, turn 90 degrees and attempt to move around object?
-			//conversely, if we move through without interuption, there must be nothing in our current location so set to 0
-				
-
-				
-			//update current location depending on movement made		
 			
-			//should we check pose as well here and try and correct our heading?
-			//opp.getHeading(); could be compared to a starting heading?
 			
 			if(//some completeness test  )
 			{
 				finished = true
 			}
 		}		
+				
 		System.exit(0);
 	}	
 }	
